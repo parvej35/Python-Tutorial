b714ce76adf0c5c0b85404e8504fcfbed5f32d77
Parvej Ahmed Chowdhury <parvej35@gmail.com>
2021-10-05 23:37:40 +0200

    Committing Tutorial-3

---
+++ Python_Tutorial_3.py
@@ -0,0 +1,35 @@
+#Casting string to number
+
+number = int("100")
+print("1a) Casting string to number: " +str(number), end='\n\n')
+#------------------------------------------------------------
+
+num = 3
+print("1) Data type: " + str(type(num)))
+
+num = 3.14
+print("2) Data type: " + str(type(num)))
+
+print("3) Abs value of -3 : " + str(abs(-3)), end='\n\n')
+
+print("4a) Round value of 3.765 : " + str(round(3.765)))
+print("4b) Round value of 3.165 : " + str(round(3.165)))
+print("4c) Round value of 3.765 till 1 decimal point : " + str(round(3.765, 1)), end='\n\n')
+
+#------------------------------------------------------------
+print("5) Arithmatic operation in python:")
+
+print("3 + 5 : " +str(3 + 5))
+print("3 - 5 : " +str(3 - 5))
+print("3 * 5 : " +str(3 * 5))
+print("3 / 5 : " +str(5 / 3), end='\n\n')
+
+print("14 // 5 : " +str(14 // 5))
+print("14 % 5 : " +str(14 % 5), end='\n\n')
+
+print("2 pow 4 : " +str(2 ** 4), end='\n\n')
+
+print("2 * 4 + 10 : " +str(2 * 4 + 10))
+print("2 * (4 + 10) : " +str(2 * (4 + 10)), end='\n\n')
+
+
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 In this Python Tutorial, we will begin learning python from A to Z. Let's get started.
 
 Tutorial 2 : String manipulation.
+Tutorial 3 : Working with Numeric Data.

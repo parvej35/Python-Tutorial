5979feabf6167c476573341179ad2c23ec7f6189
Parvej Ahmed Chowdhury <parvej35@gmail.com>
2021-10-12 14:30:39 +0200

    Add files via upload

--- Python_Tutorial_Read_File.py
+++ Python_Tutorial_Read_File.py
@@ -1,5 +1,12 @@
 
+segment_list = ["Activities", "Sleep"]
+
+sleep_array = []
+activity_array = []
+
 with open("file/fitbit_exported_data_20211011.csv") as txt_file:
+
+    print(txt_file.readlines())
 
     counter = 0
     valid = True
@@ -10,13 +17,26 @@ with open("file/fitbit_exported_data_20211011.csv") as txt_file:
 
         counter += 1
 
-        if line.strip() != "":
-            srtSegment = line.strip()
+        print(line.strip())
 
-            x = txt_file.readlines()[counter - 1:]
-            # print(x)
-            for i in x:
-                print(i)
+        # if line.strip() == "Sleep":    
+        if activity_array.count(line.strip()):
+
+            # counter = 0
+
+            # strSegment = line.strip()
+
+            data_values = txt_file.readlines()[counter - 1:]
+            print(x)
+
+            for value in data_values:
+                counter += 1
+
+                if value.strip() == "":
+                    print("This is the end of " + strSegment)
+                    break
+                else:
+                    print("-> " + value)
 
             # while valid:
                 # print(str(counter) +") "+ line)        
